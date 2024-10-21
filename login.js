bbc5b5ed7d9f71ce3ca5774416e48ff310d3fa2e
clara-sousa <claralanducci66@gmail.com>
2024-10-20 23:20:14 -0300

    Atualizar o login.js

--- login.js
+++ login.js
@@ -1,16 +1,1 @@
-document.getElementById('login').addEventListener('click', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('name').value;
-    const password = document.getElementById('password').value;
-    localStorage.setItem('name', username);
-    localStorage.setItem('password', password);
-    if (username !== "" && password !== "") {
-        if (username === "usuarioValido" && password === "senhaValida") {
-            alert("Login realizado com sucesso!");
-        } else {
-            alert("Usuário ou senha inválidos");
-        }
-    } else {
-        alert("Por favor, preencha todos os campos");
-    }
-});
+
